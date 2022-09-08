c559c543c7ed29f8573286f68ce5e4bd36673298
Evans Ngugi <ngugie649@gmail.com>
2022-09-08 13:51:16 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,3 +6,8 @@
  *   followed by a new line, to the standard error.
  *   Return: Always 0 (Success)
  */
+{
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
