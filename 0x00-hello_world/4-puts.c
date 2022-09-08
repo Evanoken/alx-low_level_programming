36767d03065224c52acfc34a9e19ebfdab50bd71
Evans Ngugi <ngugie649@gmail.com>
2022-09-08 12:41:50 -0700

    C files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
-/**
- * This is a comment
- *
- *Hello there 
- */
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
-	return (0);
+	puts("Programming is like building a multilingual puzzle \n");
+	return 0;
 }
