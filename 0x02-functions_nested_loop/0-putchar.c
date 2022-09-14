68574ff8b81dadc3e376688b793c753bedcc3509
Evans Ngugi <ngugie649@gmail.com>
2022-09-14 10:24:27 -0700

    _putchar

---
+++ 0x02-functions_nested_loop/0-putchar.c
@@ -0,0 +1,19 @@
+#include "evans.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *sh = "evans";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
