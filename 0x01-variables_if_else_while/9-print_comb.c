d7abb871268bd3b6e23a95c1cd4f3634c7e5e089
Evans Ngugi <ngugie649@gmail.com>
2022-09-19 14:50:12 -0700

    fixation

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Prints all possible combinations of single-digit numbers.
  *
@@ -8,18 +7,14 @@
 int main(void)
 {
 	int num;
-
 	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
 		if (num == 9)
 			continue;
-		
 		putchar(',');
 		putchar(' ');
 	}
-
 	putchar('\n');
-
 	return (0);
 }
