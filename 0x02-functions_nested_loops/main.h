c1df9c769624c21bb54ecea4c57f26b6083c9a68
Evans Ngugi <ngugie649@gmail.com>
2022-09-22 08:26:23 -0700

    alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,4 @@
 #ifndef MY_HEADER_FILE_H
 #define MY_HEADER_FILE_H
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
+int _putchar(char c);
 #endif
