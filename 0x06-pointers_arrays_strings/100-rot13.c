98d7d1946a65c8f178bee48f0a161233abe8faac
Evans Ngugi <ngugie649@gmail.com>
2022-09-23 16:30:00 -0700

    Advanced C language

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * rot13 - encrypts code
  * @s: string to encrypt
@@ -6,23 +8,23 @@
 
 char *rot13(char *s)
 {
-        char part1[52] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
-        char part2[52] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
+	char part1[52] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
+	char part2[52] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
 
-        int i;
-        int j = 0;
+	int i;
+	int j = 0;
 
-        for (i = 0; s[i] != '\0'; i++)
-        {
+	for (i = 0; s[i] != '\0'; i++)
+	{
 
-                for (j = 0; part1[j] != '\0'; j++)
-                {
-                        if (s[i] == part1[j])
-                        {
-                                s[i] = part2[j];
-                                break;
-                        }
-                }
-        }
-        return (s);
+		for (j = 0; part1[j] != '\0'; j++)
+		{
+			if (s[i] == part1[j])
+			{
+				s[i] = part2[j];
+				break;
+			}
+		}
+	}
+	return (s);
 }
